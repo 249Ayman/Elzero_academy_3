928a9bb468a59ef515ba1ad294425cb710a9e0ca
249Ayman <140142498+249Ayman@users.noreply.github.com>
2024-06-29 22:37:12 +0300

    .

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let btn= document.getElementById("btn")
 
 btn.onclick= function() {
-    document.getElementById("mega-links").style.opacity="1"
+    let show=document.getElementById("mega-links")
+    show.classList.toggle("mega-links")
 }
+document.addEventListener("click", function (event) {
+    if (event.target != btn) {
+        show.classList.remove("mega-links")
+    }
+})
 // btn.onclick= function() {
 //     document.getElementById("mega-links").style.opacity="0"
 
